a88bb97e53eee3440025f8469cd208a97b0d9885
Hugo <contacthugocampos@gmail.com>
2019-07-27 17:52:25 -0700

    enable express-session

--- src/server/index.js
+++ src/server/index.js
@@ -2,8 +2,7 @@ const db = require('./db');
 const express = require('express');
 const path = require('path');
 const seed = require('./db/seed');
-console.log(seed);
-
+const session = require('express-session');
 
 // Port assignment
 const port = process.env.PORT || 3000;
@@ -14,6 +13,21 @@ const app = express();
 // Body parsing middleware
 app.use(express.json());
 //app.use(express.urlencoded({extended: true}));
+
+// Session Middelware
+app.use(session({
+  secret:'iDKWhatAGoodSessionSecretIs',
+  // Reduces session concurrency issues by not resaving if you haven't changed session.
+  resave: false,
+  // If new, but not modified, still save
+  saveUninitialized: true
+}));
+
+// Session Logging Middleware
+app.use((req, res, next) => {
+  console.log('SESSION: ', req.session)
+  next()
+})
 
 // Static
 app.use('/dist', express.static(path.join(__dirname, '..', '..', 'dist')));
@@ -29,7 +43,7 @@ app.get('/', (req, res, next) => {
 // API Routes
 app.use('/api', require('./routes/api'));
 
-// Sync then Listen
+// Sync, then Seed, then Listen
 async function listen() {
   await db.sync();
   console.log('DB synced!');
