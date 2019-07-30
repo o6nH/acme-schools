9ef59df7c5e13f2e3434d23d07e5d16e0eaf9a8c
Hugo <contacthugocampos@gmail.com>
2019-07-29 19:02:28 -0700

    change header in session table

--- src/server/db/models/Session.js
+++ src/server/db/models/Session.js
@@ -1,17 +1,17 @@
 const db = require('..');
 
 const Session = db.define('session', {
-  sid: {
+  sessionId: {
     type: db.Sequelize.STRING,
     allowNull: false,
     primaryKey: true,
+    unique: true,
     validate: {
       notEmpty: true
     }
   },
-  userId: {
-    type: db.Sequelize.STRING
-  },
+  //keeping 'userId' attribute in this Sequelize Session Store/Model, instead of 'studentId', in order to emphasize that the user (a.k.a., client) need not necessarily be one of the students in our Student Table/Model, and, instead, this Sequelize Session Store/Model could have allowed non-student users in their own separate table.
+  userId: db.Sequelize.STRING,
   data: db.Sequelize.STRING(50000), //for connect-session-sequelize's defaults (extendDefaultFields)
   expires: db.Sequelize.DATE //for connect-session-sequelize's defaults
 });
