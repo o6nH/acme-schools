67770c391bd85e00e5ca3ea48a966890e76108a0
Hugo <contacthugocampos@gmail.com>
2019-07-30 01:58:44 -0700

    change `authorize` function to `authorized`

--- src/server/routes/api/index.js
+++ src/server/routes/api/index.js
@@ -3,11 +3,11 @@ const router = require('express').Router();
 /* Callback paramater is array of two middleware functions (second of which is router middleware). 
 If the first finds `userId` has a session via req.session, the second will route to a specified 
 `routePath` because next() is called, otherwise the response will end with a 401 status. */
-const authorize = routePath => [(req, res, next) => req.session.userId 
+const authorized = routePath => [(req, res, next) => req.session.userId 
   ? next() : res.status(401).end(), require(routePath)]
 
-router.use('/students', authorize('./students'));
-router.use('/schools', authorize('./schools'));
+router.use('/students', authorized('./students'));
+router.use('/schools', authorized('./schools'));
 router.use('/sessions', require('./sessions'));
 
 module.exports = router;
