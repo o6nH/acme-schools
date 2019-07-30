588a80794c8929d175e5f319bd2d0e042ac7779d
Hugo <contacthugocampos@gmail.com>
2019-07-30 01:21:05 -0700

    allow only users w/ sessionIds to access apis

--- src/server/routes/api/index.js
+++ src/server/routes/api/index.js
@@ -1,7 +1,13 @@
 const router = require('express').Router();
 
-router.use('/students', require('./students'));
-router.use('/schools', require('./schools'));
+/* Callback paramater is array of two middleware functions (second of which is router middleware). 
+If the first finds `userId` has a session via req.session, the second will route to a specified 
+`routePath` because next() is called, otherwise the response will end with a 401 status. */
+const authorize = routePath => [(req, res, next) => req.session.userId 
+  ? next() : res.status(401).end(), require(routePath)]
+
+router.use('/students', authorize('./students'));
+router.use('/schools', authorize('./schools'));
 router.use('/sessions', require('./sessions'));
 
 module.exports = router;
